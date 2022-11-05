f1669d92e9d27cad86143bb62af4d1c068432e04
Obinna <obiknows88@gmail.com>
2022-11-05 08:13:30 +0200

    got the bridge monitoring checking ethscan every 7 seconds

--- main.go
+++ main.go
@@ -2,17 +2,35 @@ package main
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/nanmu42/etherscan-api"
 )
 
-func main()  {
+var ethscan *etherscan.Client
+
+func init()  {
+	// setup the client
+	// 1. create a API client for eth mainnet (hard-coded test API key)
+	ethscan = etherscan.New(etherscan.Mainnet, "IBXNW84YSJV17GNXGVHKT6HUS1BXJYXMMC")
+
+	// startup prompt
 	fmt.Println("hello world, its me the bridge monitor")
 	fmt.Println("checking an account balance as a test....")
-	// create a API client for specified ethereum net
-	// there are many pre-defined network in package
-	ethscan := etherscan.New(etherscan.Mainnet, "IBXNW84YSJV17GNXGVHKT6HUS1BXJYXMMC")
+}
 
+func main()  {
+	// the free etherscan API has a rate-limit of 5 sec/req
+	// so... we'll poll every 7 seconds to be quick but safe
+	tick := time.Tick(7 * time.Second)
+	for range tick {
+			fmt.Println("7 second Tick")
+			checkAccountBalance()
+			checkIfOptimismBridgeIsAlive()
+	}
+}
+
+func checkAccountBalance() {
 	// check account balance
 	balance, err := ethscan.AccountBalance("0x281055afc982d96fab65b3a49cac8b878184cb16")
 	if err != nil {
@@ -21,8 +39,8 @@ func main()  {
 	// balance in wei, in *big.Int type
 	fmt.Println("account balance:", balance.Int())
 
-	
 }
 
-
-//Test API Key: IBXNW84YSJV17GNXGVHKT6HUS1BXJYXMMC
+func checkIfOptimismBridgeIsAlive() bool {
+	return true
+} 
