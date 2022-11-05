3924524c202009f0e27fbbe8b55bdbbfe0dc9e62
Obinna <obiknows88@gmail.com>
2022-11-05 07:49:51 +0200

    added in lib for etherscan and tested 👌🏿

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/obiknows/bridge-monitor
 
 go 1.18
+
+require github.com/nanmu42/etherscan-api v1.9.0 // indirect
--- main.go
+++ main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/nanmu42/etherscan-api"
+)
 
 func main()  {
 	fmt.Println("hello world, its me the bridge monitor")
+	fmt.Println("checking an account balance as a test....")
+	// create a API client for specified ethereum net
+	// there are many pre-defined network in package
+	ethscan := etherscan.New(etherscan.Mainnet, "IBXNW84YSJV17GNXGVHKT6HUS1BXJYXMMC")
+
+	// check account balance
+	balance, err := ethscan.AccountBalance("0x281055afc982d96fab65b3a49cac8b878184cb16")
+	if err != nil {
+		panic(err)
+	}
+	// balance in wei, in *big.Int type
+	fmt.Println("account balance:", balance.Int())
+
+	
 }
+
+
+//Test API Key: IBXNW84YSJV17GNXGVHKT6HUS1BXJYXMMC
