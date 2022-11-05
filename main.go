6daaa1bd0b3dffef68ab4ad79f95af1a906b4c56
Obinna <obiknows88@gmail.com>
2022-11-05 10:09:37 +0200

    added majority of bridge monitoring system

--- main.go
+++ main.go
@@ -4,43 +4,78 @@ import (
 	"fmt"
 	"time"
 
+	// "time"
+
 	"github.com/nanmu42/etherscan-api"
 )
 
 var ethscan *etherscan.Client
+var CTCAddress string
+var MaxAllowableTimeDifference time.Duration
 
 func init()  {
 	// setup the client
 	// 1. create a API client for eth mainnet (hard-coded test API key)
 	ethscan = etherscan.New(etherscan.Mainnet, "IBXNW84YSJV17GNXGVHKT6HUS1BXJYXMMC")
 
+	// 2. fill in constants
+	CTCAddress = "0x5E4e65926BA27467555EB562121fac00D24E9dD2" // address for the CTC
+	MaxAllowableTimeDifference = 2 * time.Minute // use a max 2 minute window
+
 	// startup prompt
-	fmt.Println("hello world, its me the bridge monitor")
-	fmt.Println("checking an account balance as a test....")
+	fmt.Println("The Optimism Bridge Monitor 🌉🖥️")
+	fmt.Println("--------------------------------")
+	fmt.Println("Prints 'True' is bridge is alive.")
+	fmt.Println("Prints 'False' if bridge is down.")
+	fmt.Print("Prints 'Error' with a message and exits if there is an error with bridge monitoring system.\n\n\n")
 }
 
 func main()  {
 	// the free etherscan API has a rate-limit of 5 sec/req
-	// so... we'll poll every 7 seconds to be quick but safe
-	tick := time.Tick(7 * time.Second)
+	// so... we'll poll every 10 seconds to be quick but safe
+	tick := time.Tick(10 * time.Second)
 	for range tick {
-			fmt.Println("7 second Tick")
-			checkAccountBalance()
-			checkIfOptimismBridgeIsAlive()
+		alive, err := checkIfOptimismBridgeIsAlive()
+		if err != nil {
+			fmt.Println("⚠️ Error:", err.Error())
+			fmt.Println("Program Exiting.")
+			break
+		}
+		if alive {
+			fmt.Println("True")
+		} else {
+			fmt.Println("False")
+		}
 	}
 }
 
-func checkAccountBalance() {
-	// check account balance
-	balance, err := ethscan.AccountBalance("0x281055afc982d96fab65b3a49cac8b878184cb16")
+
+func checkCTCTransctionsTimeDifference() (time.Duration, error) {
+	// ERC20 transactions from/to a specified address
+	txs, err := ethscan.NormalTxByAddress(CTCAddress, nil, nil, 1, 5, true)
 	if err != nil {
-		panic(err)
-	}
-	// balance in wei, in *big.Int type
-	fmt.Println("account balance:", balance.Int())
+		err = fmt.Errorf("there was an error getting tx's from the CTC. please check your")
+		return 0, err
+	} 
+		// get most recent and oldest tx's from window...
+		mostRecentTxTime := txs[0].TimeStamp.Time()
+		oldestTxTime := txs[len(txs)-1].TimeStamp.Time()
+		// .. calculate the time difference and return
+		timeDiff := mostRecentTxTime.Sub(oldestTxTime)
+
+		return timeDiff, nil
 
 }
 
-func checkIfOptimismBridgeIsAlive() bool {
-	return true
+func checkIfOptimismBridgeIsAlive() (bool, error) {
+	timeDiff, err := checkCTCTransctionsTimeDifference()
+	if err != nil {
+		return false, err
+	}
+
+	if (timeDiff > MaxAllowableTimeDifference) {
+		return false, err
+	}
+
+	return true, err
 } 
